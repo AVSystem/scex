849cd5fe5811331a7eaeb181111c8024ec85c3d9
Roman Janusz <romeqjanoosh@gmail.com>
2014-02-07 16:48:11 +0100

    removed caching from TypeInfo that could cause potential memory leaks
    --HG--
    branch : 0.9

--- scex-core/src/main/scala/com/avsystem/scex/validation/TypeInfo.scala
+++ scex-core/src/main/scala/com/avsystem/scex/validation/TypeInfo.scala
@@ -1,20 +1,12 @@
 package com.avsystem.scex.validation
 
-import com.avsystem.scex.util.CommonUtils
-import com.google.common.cache.CacheBuilder
 import java.{util => ju, lang => jl}
 import reflect.api.{Universe, TypeCreator}
 
-
 class TypeInfo(typeCreator: TypeCreator, val clazz: Option[Class[_]], val isJava: Boolean, typeRepr: String) {
 
-  import CommonUtils._
-
-  // a double-entry cache (universe passed to typeIn will probably always be the compiler or presentation compiler)
-  private val cache = CacheBuilder.newBuilder.weakKeys.maximumSize(2).build[Universe, Universe#Type]
-
   def typeIn(u: Universe): u.Type =
-    cache.get(u, callable(u.TypeTag[Any](u.rootMirror, typeCreator).tpe)).asInstanceOf[u.Type]
+    u.TypeTag[Any](u.rootMirror, typeCreator).tpe
 
   override def toString = typeRepr
 }
