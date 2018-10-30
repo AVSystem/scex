16c7e4aaddbc000c092862377a75397532cc4f5c
Łukasz Pożarlik <lpozarlik@gmail.com>
2018-10-30 16:10:21 +0100

    Additional methods for net context:
    * networkAddress(ipWithMask)
    * getMask(cidr)
    * getMinAddress(ip, mask)
    * getMaxAddress(ip, mask)

--- scex-util/src/main/scala/com/avsystem/scex/util/function/NetUtil.java
+++ scex-util/src/main/scala/com/avsystem/scex/util/function/NetUtil.java
@@ -4,19 +4,34 @@ import com.avsystem.scex.presentation.annotation.Documentation;
 import com.avsystem.scex.presentation.annotation.ParameterNames;
 
 public interface NetUtil {
-    @ParameterNames({ "ip", "subnetWithMask" })
+    @ParameterNames({"ip", "subnetWithMask"})
     boolean isIpInSubnet(String ip, String subnetWithMask);
 
-    @ParameterNames({ "ip", "subnet", "mask" })
+    @ParameterNames({"ip", "subnet", "mask"})
     boolean isIpInSubnetWithMask(String ip, String subnet, String mask);
 
-    @ParameterNames({ "ip", "mask" })
+    @ParameterNames({"ip", "mask"})
     String networkAddress(String ip, String mask);
+
+    @ParameterNames({"subnetWithMask"})
+    String networkAddress(String subnetWithMask);
+
+    @ParameterNames({"cidr"})
+    @Documentation("return network mask from CIDR in /XX")
+    String getMask(String cidr);
+
+    @ParameterNames({"ip", "mask"})
+    @Documentation("return minimal host address for given network and mask")
+    String getMinAddress(String ip, String mask);
+
+    @ParameterNames({"ip", "mask"})
+    @Documentation("return maximal host address for given network and mask")
+    String getMaxAddress(String ip, String mask);
 
     @ParameterNames("mac")
     String stripMac(String mac);
 
-    @ParameterNames({ "ip1", "ip2" })
+    @ParameterNames({"ip1", "ip2"})
     Integer compareIp(String ip1, String ip2);
 
     @ParameterNames("ip")
