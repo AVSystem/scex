60783a299fcd56b27edbca7efe9e0bcaf33fe4e0
Roman Janusz <r.janusz@avsystem.com>
2022-02-02 11:55:03 +0100

    added simple protection against most likely binary incompatibilities in ContextTypeInfo

--- scex-core/src/main/scala/com/avsystem/scex/compiler/ContextTypeInfo.scala
+++ scex-core/src/main/scala/com/avsystem/scex/compiler/ContextTypeInfo.scala
@@ -1,9 +1,18 @@
 package com.avsystem.scex.compiler
 
+import com.avsystem.commons.annotation.bincompat
 import com.avsystem.scex.{ExpressionContext, Macros}
 
-case class ContextTypeInfo[C](fullTypeString: String, rootObjectClass: Class[_])
+case class ContextTypeInfo[C](fullTypeString: String, rootObjectClass: Class[_]) {
+  @bincompat
+  private[compiler] def this(fullTypeString: String, rootObjectClassName: String) =
+    this(fullTypeString, Class.forName(rootObjectClassName))
+}
 object ContextTypeInfo {
   // Scala, seriously, why do I have to do this with a macro?
   implicit def typeInfo[C <: ExpressionContext[_, _]]: ContextTypeInfo[C] = macro Macros.mkContextTypeInfo[C]
+
+  @bincompat
+  private[compiler] def apply[C](fullTypeString: String, rootObjectClassName: String): ContextTypeInfo[C] =
+    new ContextTypeInfo(fullTypeString, rootObjectClassName)
 }
