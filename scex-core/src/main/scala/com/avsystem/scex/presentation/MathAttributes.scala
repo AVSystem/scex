cb2ff8850fcd787a00ac5b4734118cd5f9432ad4
Mateusz <m.krol@avsystem.com>
2023-08-23 12:22:03 +0200

    math util descriptions review

--- scex-core/src/main/scala/com/avsystem/scex/presentation/MathAttributes.scala
+++ scex-core/src/main/scala/com/avsystem/scex/presentation/MathAttributes.scala
@@ -5,8 +5,7 @@ import com.avsystem.scex.symboldsl.SymbolInfo
 
 object MathAttributes {
   final val scalaMathAttributes: List[SymbolInfo[Attributes]] = attributes {
-    on { m: scala.math.type =>
-
+    on { m: math.`package`.type =>
       m.IEEEremainder _ --> Attributes(documentation = "Returns the remainder resulting from the division of a specified number `x` by another specified number `y`.")
 
       m.abs(_: Double) --> Attributes(documentation = "Returns absolute value of `x` argument.")
@@ -36,20 +35,16 @@ object MathAttributes {
       m.decrementExact(_: Long) --> Attributes(documentation = "Returns the `x` argument decremented by one.")
 
       m.exp _ --> Attributes(documentation = "Returns the Euler's number raised to the power of the `x` argument.")
-      m.expm1 _ --> Attributes(documentation = "Return the Euler's number raised to the power of the `x` argument with one subtracted from it.")
+      m.expm1 _ --> Attributes(documentation = "Returns `exp(x) - 1`, the Euler's number raised to the power of the `x` argument minus one.")
 
       m.floor _ --> Attributes(documentation = "Returns the largest integer value less than or equal to the `x` argument.")
-      m.floorDiv(_: Int, _:Int) --> Attributes(documentation = "Returns the largest integer value less than or equal to the algebraic quotient, " +
-        "where `x` is the dividend and `y` is the divisor.")
-      m.floorDiv(_: Long, _:Long) --> Attributes(documentation = "Returns the largest integer value less than or equal to the algebraic quotient, " +
-        "where `x` is the dividend and `y` is the divisor.")
-      m.floorMod(_: Int, _:Int) --> Attributes(documentation = "Returns the largest integer value less than or equal to the algebraic quotient, " +
-        "where `x` is the dividend and `y` is the divisor.")
-      m.floorMod(_: Long, _:Long) --> Attributes(documentation = "Returns the largest integer value less than or equal to the algebraic quotient, " +
-        "where `x` is the dividend and `y` is the divisor.")
+      m.floorDiv(_: Int, _: Int) --> Attributes(documentation = "Returns the largest integer value less than or equal to the algebraic quotient, " + "where `x` is the dividend and `y` is the divisor.")
+      m.floorDiv(_: Long, _: Long) --> Attributes(documentation = "Returns the largest integer value less than or equal to the algebraic quotient, " + "where `x` is the dividend and `y` is the divisor.")
+      m.floorMod(_: Int, _: Int) --> Attributes(documentation = "Returns the floor modulus of the provided arguments, " + "where `x` is the dividend and `y` is the divisor.")
+      m.floorMod(_: Long, _: Long) --> Attributes(documentation = "Returns the floor modulus of the provided arguments, " + "where `x` is the dividend and `y` is the divisor.")
 
-      m.getExponent(_: Double) --> Attributes(documentation = "Returns the size of unit in the last place of the `d` argument.")
-      m.getExponent(_: Float) --> Attributes(documentation = "Returns the size of unit in the last place of the `f` argument.")
+      m.getExponent(_: Double) --> Attributes(documentation = "Returns the unbiased exponent used in the representation of the `d` argument.")
+      m.getExponent(_: Float) --> Attributes(documentation = "Returns the unbiased exponent used in the representation of the `f` argument.")
 
       m.hypot _ --> Attributes(documentation = "Returns the square root of the sum of the squares of both `x` and `y` arguments without intermediate underflow or overflow.")
 
@@ -60,10 +55,10 @@ object MathAttributes {
       m.log10 _ --> Attributes(documentation = "Returns base 10 natural logarithm of the `x` argument.")
       m.log1p _ --> Attributes(documentation = "Returns the natural logarithm of the sum of the `x` argument and 1")
 
-      m.max(_: Double, _:Double) --> Attributes(documentation = "Returns the greater of the provided `x` and `y` arguments.")
-      m.max(_: Float, _:Float) --> Attributes(documentation = "Returns the greater of the provided `x` and `y` arguments.")
-      m.max(_: Int, _:Int) --> Attributes(documentation = "Returns the greater of the provided `x` and `y` arguments.")
-      m.max(_: Long, _:Long) --> Attributes(documentation = "Returns the greater of the provided `x` and `y` arguments.")
+      m.max(_: Double, _: Double) --> Attributes(documentation = "Returns the greater of the provided `x` and `y` arguments.")
+      m.max(_: Float, _: Float) --> Attributes(documentation = "Returns the greater of the provided `x` and `y` arguments.")
+      m.max(_: Int, _: Int) --> Attributes(documentation = "Returns the greater of the provided `x` and `y` arguments.")
+      m.max(_: Long, _: Long) --> Attributes(documentation = "Returns the greater of the provided `x` and `y` arguments.")
 
       m.min(_: Double, _: Double) --> Attributes(documentation = "Returns the smaller of the provided `x` and `y` arguments.")
       m.min(_: Float, _: Float) --> Attributes(documentation = "Returns the smaller of the provided `x` and `y` arguments.")
@@ -84,28 +79,26 @@ object MathAttributes {
       m.nextUp(_: Float) --> Attributes(documentation = "Returns the floating-point value adjacent to the `f` argument in the direction of positive infinity.")
 
       m.pow(_: Double, _: Double) --> Attributes(documentation = "Returns the value of the `x` argument raised to the power of the the `y` argument. ")
-      m.rint _ --> Attributes(documentation = "Returns the closest floating point value that is equal to the integer for the `x` argument.")
+      m.rint _ --> Attributes(documentation = "Returns the closest floating point value that is equal to a mathematical integer for the `x` argument.")
 
       m.round(_: Double) --> Attributes(documentation = "Returns the `x` argument rounded to the closest integer value.")
       m.round(_: Float) --> Attributes(documentation = "Returns the `x` argument rounded to the closest integer value.")
       m.round(_: Long) --> Attributes(documentation = "Returns the `x` argument rounded to the closest integer value.")
 
-      m.scalb(_: Double, _: Int) --> Attributes(documentation = "Returns `d` times 2 to the power of `scaleFactor` " +
-        "rounded as if performed by a single correctly rounded floating-point multiply.")
-      m.scalb(_: Float, _: Int) --> Attributes(documentation = "Returns `f` times 2 to the power of `scaleFactor` " +
-        "rounded as if performed by a single correctly rounded floating-point multiply.")
+      m.scalb(_: Double, _: Int) --> Attributes(documentation = "Returns `d` times 2 to the power of `scaleFactor` " + "rounded as if performed by a single correctly rounded floating-point multiply.")
+      m.scalb(_: Float, _: Int) --> Attributes(documentation = "Returns `f` times 2 to the power of `scaleFactor` " + "rounded as if performed by a single correctly rounded floating-point multiply.")
 
-      m.signum (_: Double) --> Attributes(documentation = "Returns the signum function of the `x` argument.")
-      m.signum (_: Float) --> Attributes(documentation = "Returns the signum function of the `x` argument.")
-      m.signum (_: Int) --> Attributes(documentation = "Returns the signum function of the `x` argument.")
-      m.signum (_: Long) --> Attributes(documentation = "Returns the signum function of the `x` argument.")
+      m.signum(_: Double) --> Attributes(documentation = "Returns the signum function of the `x` argument.")
+      m.signum(_: Float) --> Attributes(documentation = "Returns the signum function of the `x` argument.")
+      m.signum(_: Int) --> Attributes(documentation = "Returns the signum function of the `x` argument.")
+      m.signum(_: Long) --> Attributes(documentation = "Returns the signum function of the `x` argument.")
 
       m.sin _ --> Attributes(documentation = "Returns the sine of the `x` argument.")
       m.sinh _ --> Attributes(documentation = "Returns the hyperbolic sine of the `x` argument.")
       m.sqrt _ --> Attributes(documentation = "Returns the square root of the `x` argument.")
 
-      m.subtractExact(_:Int, _:Int) --> Attributes(documentation = "Returns the difference of the `x` and `y` arguments.")
-      m.subtractExact(_:Long, _:Long) --> Attributes(documentation = "Returns the difference of the `x` and `y` arguments.")
+      m.subtractExact(_: Int, _: Int) --> Attributes(documentation = "Returns the difference of the `x` and `y` arguments.")
+      m.subtractExact(_: Long, _: Long) --> Attributes(documentation = "Returns the difference of the `x` and `y` arguments.")
 
       m.tan _ --> Attributes(documentation = "Returns the tangent of the `x` argument.")
       m.tanh _ --> Attributes(documentation = "Returns the hyperbolic tangent of the `x` argument.")
